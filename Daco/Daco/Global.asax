81a64d1e5700817c437a408afad0cd4c62dad568
Lukas Simon <lukas.simon@vixtechnology.com>
2017-02-20 19:36:06 +0100

    testovanie dacoho

---
+++ Daco/Daco/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Daco.MvcApplication" Language="C#" %>
---
+++ Daco/Daco/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Daco.Startup))]
+namespace Daco
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
