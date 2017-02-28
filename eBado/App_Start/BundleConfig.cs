6586298f6a3003191216a654e2c70fcb2d252742
Svoboda <Svoboda@SVOBODA>
2017-02-28 20:34:24 +0100

    Add project files.

---
+++ eBado/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace eBado
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Assets/js/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Assets/js/bootstrap.js",
+                      "~/Assets/js/bootstrap-hover-dropdown.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Assets/css/bootstrap.css",
+                      "~/Assets/css/style.css",
+                      "~/Assets/css/button.css",
+                      "~/Assets/css/reset.css",
+                      "~/Assets/css/font-awesome.min.css",
+                      "~/Assets/css/media-queries.css"));
+        }
+    }
+}
---
+++ eBado/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using eBado.Models;
+
+namespace eBado.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+      
+    }
+}
