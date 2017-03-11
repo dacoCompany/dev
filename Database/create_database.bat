534a4d8ce90780478027cba8aa297e6cc702375f
Lukas Simon <lukas.simon@vixtechnology.com>
2017-03-11 21:39:03 +0100

    Created class library Infrastructure.Common.DB for object related mapping with repository and uow pattern and EF. Created class library Infrastructure.Common for common enums,data helpers etc.

---
+++ Database/create_database.bat
@@ -0,0 +1,6 @@
+@echo off
+set /p password="Enter password for sa account: "
+echo Starting create database script
+sqlcmd -S localhost\masterdb -U sa -P %password% -i Update\create_script.sql
+echo Creation of database completed
+pause
---
+++ Infrastructure.Common.DB/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Common.DB
+{
+    /// <summary>
+    /// Interface of repository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T: class
+    {
+        IQueryable<T> FindWhere(Expression<Func<T, bool>> predicate);
+        IQueryable<T> FindAll();
+        T FindById(int id);
+        T Add(T obj);
+        IEnumerable<T> AddRange(IEnumerable<T> objCollection);
+        void Delete(T obj);
+    }
+}
